46656b4255235109638953d82630b90b27206bb9
Chen <huansheng.chen@rakuten.com>
2020-05-05 15:17:26 +0900

    initial

---
+++ src/main/java/com/rakuten/dsaid/sonar/SonarJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rakuten.dsaid.sonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarJavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarJavaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakuten/dsaid/sonar/SonarJavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakuten.dsaid.sonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarJavaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
